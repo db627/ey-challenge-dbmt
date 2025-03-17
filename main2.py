46f21c0218764bd30977ce65c0b8d46dddc03f7e
Dennis Boguslavskiy <db627@njit.edu>
2025-03-17 18:44:17 -0400

    updates

--- main2.py
+++ main2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-
+#hello
 def main():
     # ---------------------------
     # 1. Load and Prepare Training Data
